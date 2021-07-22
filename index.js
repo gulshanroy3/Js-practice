aca7c73d22aafd61e8266afdcdfeb1b49dc2c578
gulsha kumar <gulshananmol4@gmail.com>
2021-07-22 11:23:52 +0530

    class2

--- index.js
+++ index.js
@@ -1,4 +1,33 @@
-/**
- * UI=>API=>DB
- * html=>css=>js
- */
+let buttonJS = document.getElementById("button-js")
+let inputJS = document.getElementById("input-js")
+let i = 1;
+// buttonJS.addEventListener('click', onbuttonClick)
+// inputJS.addEventListener('input', function (e) {
+//     console.log(e)
+
+// })
+// function onbuttonClick(e) {
+//     // console.log(e)
+//     buttonJS.innerHTML = 'this is button' + i
+//     i++;
+
+// }
+//function, arrow function
+// const buttonClick = (args) => {
+//     console.log("clicked", args)
+// }
+function buttonClick() {
+    buttonJS.innerHTML = 'this is button' + i
+    i++;
+}
+function onInputChange(value) {
+    if (value === 'suma') inputJS.className = 'for-input'
+    else {
+        if (inputJS.className !== 'white-color') {
+            inputJS.className = 'white-color'
+        }
+    }
+}
+
+//debounce-> last input next input difference x time dont call function
+//throttle-> difference btw last work and next work should be x ms.
